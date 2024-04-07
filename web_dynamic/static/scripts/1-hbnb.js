a4283af369cf79c0982e886100da06c3c298bf8a
LWSSIM <wassim.elmnajja2@gmail.com>
2024-04-07 02:45:47 +0000

    cleanup

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   const amenity = {};
 
   $('li input[type=checkbox]').change(function () {
-    const count = $('li input[type=checkbox]:checked').length;
     if (this.checked) {
       amenity[$(this).data('id')] = $(this).data('name');
     } else {
@@ -12,9 +11,9 @@ $(document).ready(function () {
 
     if (names.length === 0) {
       $('.amenities h4').html('&nbsp;');
-    } else if (names.length > 0 && count <= 3) {
+    } else if (names.length > 0 && names.length <= 3) {
       $('.amenities h4').text(names.sort().join(', '));
-    } else if (count > 3) {
+    } else if (names.length > 3) {
       $('.amenities h4').text(names.sort().slice(0, 3).join(', ') + '...');
     }
   });
